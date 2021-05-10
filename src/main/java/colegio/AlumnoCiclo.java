7b8b407c728c16039d734cadfcfe6b5854a3fbfe
Rafael <rafa_bv95@outlook.es>
2021-05-10 09:55:41 +0200

    Readme.md instituto

--- src/main/java/colegio/AlumnoCiclo.java
+++ src/main/java/colegio/AlumnoCiclo.java
@@ -4,23 +4,53 @@ import java.util.Date;
 
 public final class AlumnoCiclo extends Alumno {
 
+   /**
+    * Constructor por defecto de alumno 
+    */
+
     public AlumnoCiclo() {
         super();
     }
+    
+    /**
+    * Constructor de alumno 
+    * @param nombre Nombre del alumno 
+    * @param apellidos apellidos del alumno 
+    */
 
     public AlumnoCiclo(String nombre, String apellidos) {
         super(nombre, apellidos);
     }
 
+
+     /**
+    * Constructor de alumno 
+    * @param nombre Nombre del alumno 
+    * @param apellidos apellidos del alumno 
+    * @param nacimiento nacimiento del alumno 
+    * @param notas notas del alumno 
+    */
+
     public AlumnoCiclo(String nombre, String apellidos, Date nacimiento, int[] notas) {
         super(nombre, apellidos, nacimiento, notas);
     }
+
+   /**
+    * Matriculación de un alumno  
+    * @param curso curso del alumno 
+    * Si es uno de los cursos indicados en el if le asigna un curso al alumno
+    */
       
     @Override
     public void matricular(Curso curso) {
         if ( curso == Curso.SMR1 || curso == Curso.SMR2 || curso == Curso.ASIR1 || curso == Curso.ASIR2)
             this.curso = curso;
     }
+
+    /**
+    * desmatricular de un alumno  
+    *  Si el curso es distinto de null desmatricula al alumno
+    */
 
     @Override
     public void desmatricular() {
